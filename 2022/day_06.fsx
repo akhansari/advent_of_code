9fb0ddbb6f7cf2939b69e31b806d177e2585d4c9
Amin Khansari <akhansari@users.noreply.github.com>
2022-12-06 15:45:40 +0100

    day 06 refactor

--- 2022/day_06.fsx
+++ 2022/day_06.fsx
@@ -1,19 +1,22 @@
+open System
+open System.Collections.Generic
+
 let input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb
 bvwbjplbgvbhsrlpgdmjqwftvncz
 nppdvjthqldpwncqszvftbrmjlhg
 nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg
 zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
 
+let rec private find length (span: ReadOnlySpan<char>) i =
+    let hs = HashSet()
+    if span.Slice(i, length).ToArray() |> Array.exists (hs.Add >> not)
+    then find length span (i + 1)
+    else i + length
+
 let findMarker length (signal: string) =
-    let hs = System.Collections.Generic.HashSet()
-    let rec find i =
-        if Seq.exists (hs.Add >> not) signal[i..i+length-1] then
-            hs.Clear()
-            find (i + 1)
-        else
-            i + length
-    find 0
+    let span = signal.AsSpan()
+    find length span 0
 
 let data = input.Split '\n'
-data |> Array.map (findMarker  4) // Part One
-data |> Array.map (findMarker 14) // Part Two
+data |> Array.map (findMarker  4) |> printfn "%A" // Part One
+data |> Array.map (findMarker 14) |> printfn "%A" // Part Two
