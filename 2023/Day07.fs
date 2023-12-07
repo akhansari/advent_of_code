c1d4df6e5e8629dccbe44c501ff4032fac5df099
Amin Khansari <akhansari@users.noreply.github.com>
2023-12-07 22:46:39 +0100

    refactor

--- 2023/Day07.fs
+++ 2023/Day07.fs
@@ -11,13 +11,13 @@ type Hand =
     | FourKind
     | FiveKind
 
-let handOf toValue fix (cards: string) =
+let hand mapCard fixCounts cards =
     cards
     |> Seq.toList
     |> List.countBy id
-    |> List.sortByDescending (fun (card, length) -> length, toValue card)
+    |> List.sortByDescending (fun (card, length) -> length, mapCard card)
     |> List.map snd
-    |> fix
+    |> fixCounts
     |> function
     | [ 5 ]       -> FiveKind
     | [ 4; 1 ]    -> FourKind
@@ -31,30 +31,30 @@ module PartOne =
     let mapCard = function
         | 'T' -> 10 | 'J' -> 11 | 'Q' -> 12 | 'K' -> 13 | 'A' -> 14
         | c -> Char.GetNumericValue c |> int
-    let handOf =
-        handOf mapCard id
-    let sortMap (cards: string) =
-        handOf cards, cards |> Seq.map (mapCard >> sprintf "%02i") |> String.concat ""
+    let hand =
+        hand mapCard id
+    let mapSort cards =
+        hand cards, cards |> Seq.map (mapCard >> sprintf "%02i") |> String.concat ""
 
 module PartTwo =
     let mapCard = function
         | 'T' -> 10 | 'J' -> 1 | 'Q' -> 12 | 'K' -> 13 | 'A' -> 14
         | c -> Char.GetNumericValue c |> int
-    let handOf (cards: string) =
+    let hand (cards: string) =
         let jokers = cards |> Seq.filter ((=) 'J') |> Seq.length
         let fix = function first :: tail -> first+jokers :: tail | [] -> [ 5 ]
-        cards.Replace("J", "") |> handOf mapCard fix
-    let sortMap (cards: string) =
-        handOf cards, cards |> Seq.map (mapCard >> sprintf "%02i") |> String.concat ""
+        cards.Replace("J", "") |> hand mapCard fix
+    let mapSort (cards: string) =
+        hand cards, cards |> Seq.map (mapCard >> sprintf "%02i") |> String.concat ""
 
-let run sortMap lines =
+let run mapSort lines =
     lines
     |> Array.map (fun line ->
         let parts = split " " line
         parts[0], int parts[1])
-    |> Array.sortBy (fst >> sortMap)
+    |> Array.sortBy (fst >> mapSort)
     |> Array.fold (fun (sum, rank) (_, bids) -> sum+(bids*rank), rank+1) (0, 1)
     |> fst
 
-let runPartOne = run PartOne.sortMap
-let runPartTwo = run PartTwo.sortMap
+let runPartOne = run PartOne.mapSort
+let runPartTwo = run PartTwo.mapSort
