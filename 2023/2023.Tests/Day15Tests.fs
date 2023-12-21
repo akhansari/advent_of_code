1a89e64c6d2500f7cc5f8852936bec7c2de31c92
Amin Khansari <akhansari@users.noreply.github.com>
2023-12-21 21:59:41 +0100

    day 15

---
+++ 2023/2023.Tests/Day15Tests.fs
@@ -0,0 +1,18 @@
+﻿module Day15.Tests
+
+open Xunit
+
+let sample =
+    "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
+
+[<Fact>]
+let ``Compute hash`` () =
+    52 =! (hashOf "HASH")
+
+[<Fact>]
+let ``Part one`` () =
+    1320 =! (sample |> runPartOne)
+
+[<Fact>]
+let ``Part two`` () =
+    145 =! (sample |> runPartTwo)
---
+++ 2023/Day15.fs
@@ -0,0 +1,33 @@
+﻿module Day15
+
+open System.Collections.Generic
+open System.Collections.Specialized
+
+let hashOf = Seq.fold (fun res c ->
+    (res + int c) * 17 % 256) 0
+
+let runPartOne content =
+    content |> split "," |> Array.sumBy hashOf
+
+let runPartTwo (content: string) =
+    let boxes =
+        seq {0..256}
+        |> Seq.map (fun i -> KeyValuePair(i, OrderedDictionary()))
+        |> Dictionary
+
+    for step in content.Replace("-", "").Split(",") do
+        match split "=" step with
+        | [| lensLabel; focalLength |] ->
+            boxes[hashOf lensLabel][lensLabel] <- int focalLength
+        | [| lensLabel |] ->
+            boxes[hashOf lensLabel].Remove lensLabel
+        | _ ->
+            failwith "bad input"
+
+    boxes
+    |> Seq.sumBy (fun box ->
+        box.Value.Values
+        |> Seq.cast<int>
+        |> Seq.indexed
+        |> Seq.sumBy (fun (i, focalLength) ->
+            (box.Key+1) * (i+1) * focalLength))
